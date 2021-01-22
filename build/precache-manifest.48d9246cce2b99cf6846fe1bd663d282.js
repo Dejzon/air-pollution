23e75e2f459a9f776a306d33ead4dc1b7a925df5
Martina <martinapantelic360@gmail.com>
2021-01-21 20:12:26 -0600

    test

---
+++ build/precache-manifest.48d9246cce2b99cf6846fe1bd663d282.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd3c86cc36cb7af3a80355233b3f440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576cf1dca9f6e7264023",
+    "url": "/static/css/2.9e5162d3.chunk.css"
+  },
+  {
+    "revision": "fb994674b2810c02e9f0",
+    "url": "/static/css/main.7eaf15a1.chunk.css"
+  },
+  {
+    "revision": "576cf1dca9f6e7264023",
+    "url": "/static/js/2.0938a29d.chunk.js"
+  },
+  {
+    "revision": "752d80a741ebcb735717949f373ff4bc",
+    "url": "/static/js/2.0938a29d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb994674b2810c02e9f0",
+    "url": "/static/js/main.2d4063a8.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  },
+  {
+    "revision": "a01c83bc10540aa841706686cbd6ddb1",
+    "url": "/static/media/citizens_masks.a01c83bc.jpg"
+  },
+  {
+    "revision": "cd45080d5b96c2005baca0447dfb6b7e",
+    "url": "/static/media/close.cd45080d.svg"
+  },
+  {
+    "revision": "e2c5f398c836f5f057dbb4e04fd9d7c9",
+    "url": "/static/media/compass.e2c5f398.svg"
+  },
+  {
+    "revision": "d5ddfdd7148538ada5b4cc609838a64f",
+    "url": "/static/media/fabrics.d5ddfdd7.jpg"
+  },
+  {
+    "revision": "d9f0a7c8bd21cbf5fb096c14dae8f4b1",
+    "url": "/static/media/menu-icon.d9f0a7c8.svg"
+  }
+]);
